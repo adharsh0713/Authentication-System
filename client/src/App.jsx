d28262b7091dab9c3a4d541f12e2e65f1edb2f90
Adharsh <adharshbaswaraj@gmail.com>
2025-11-11 07:21:37 +0530

    Added Home Page and components Header and Navbar .jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path = '/' element = {<Home/>} />
+        <Route path = '/login' element = {<Login/>} />
+        <Route path = '/email-verify' element = {<EmailVerify/>} />
+        <Route path = '/reset-password' element = {<ResetPassword/>} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div>
+      <h1>E-mail Verification Page</h1>
+    </div>
+  )
+}
+
+export default EmailVerify
